63a90db067bfc25424b2e3b4ef0371cc77569f13
Avril Lavigne <avril1225happy@gmail.com>
2022-12-31 13:04:13 +0900

    no message

--- apps/app/management/commands/superuser.py
+++ apps/app/management/commands/superuser.py
@@ -6,9 +6,9 @@ User = get_user_model()
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        print("スーパーユーザーテスト")
         if not User.objects.filter(email=settings.SUPERUSER_EMAIL).exists():
             User.objects.create_superuser(
-                name=settings.SUPERUSER_NAME,
                 email=settings.SUPERUSER_EMAIL,
                 password=settings.SUPERUSER_PASSWORD
             )
